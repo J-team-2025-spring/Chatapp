64fd56ac3d7069538d513c8cd44a4cb18224f206
Shupo00 <su.op.0678@gmail.com>
2025-04-22 00:06:44 +0900

    shupo

--- SHABERIBA/app.py
+++ SHABERIBA/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'SHABERIBA'
+    return render_template('index.html')
 
 
 
