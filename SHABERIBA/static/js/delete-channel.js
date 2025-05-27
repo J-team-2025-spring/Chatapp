ce17d35cf6f83c781f74e5a958465b28aef78d01
seibo-hub <airwhite2525@gmail.com>
2025-05-28 02:54:39 +0900

    cancelボタン押された時の修正その2

--- SHABERIBA/static/js/delete-channel.js
+++ SHABERIBA/static/js/delete-channel.js
@@ -22,11 +22,11 @@ window.addEventListener("DOMContentLoaded", () => {
         form.method = "POST";
         
         deleteModal.style.display = "none";
-        targetToDelete = "null";
+        targetToDelete = null;
     })
 
     cancelDeleteBtn.addEventListener("click", () => {
         deleteModal.style.display = "none";
-        // targetToDelete = "null";
+        targetToDelete = null;
     })
 });
